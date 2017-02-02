6a4af9e1b24075d038da65752b9cc220cf546a11
Jinguang Liu <jinguang.liu@qq.com>
2017-02-01 21:37:20 -0500

    add WordCount example

---
+++ hadoopstudy/src/main/java/com/jliu/hadoopstudy/App.java
@@ -0,0 +1,13 @@
+package com.jliu.hadoopstudy;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
